ecba1de2adec98c40e2218f0cb5fb3eb2823b3bc
varma1959-git <herovarma1959@gmail.com>
2025-09-26 15:56:13 +0530

    First commit - Streamlit Student Dashboard

---
+++ constants.py
@@ -0,0 +1,10 @@
+# config/constants.py
+
+# Path to main data file
+DATA_FILE = "db/data.csv"
+
+# Path to history log file
+HISTORY_FILE = "db/history.csv"
+
+# Default columns for the dataset
+COLUMNS = ["Name", "Age", "Score"]
